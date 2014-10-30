24ea4f9c4f092c6340337b60de0fce6244c34ce0
Brody Dunn <brody.dunn@atlantishealthcare.com>
2014-10-30 16:23:30 +1300

    Fix #5 by validating that date meets the ISO8601 full-date spec.

--- tests/testDateValidation.js
+++ tests/testDateValidation.js
@@ -88,5 +88,71 @@ module.exports.validationTests = {
         test.ok(errors.valid);
 
         test.done();
+    },
+    validateDateFormatOk: function(test) {
+        var data = {
+            "salutation": "Mr Death",
+            "dateOfBirth": "2014-01-01"
+        };
+        var model = {
+            properties: {
+                dateOfBirth: {
+                    type: "string",
+                    format: 'date'
+                }
+            }
+        };
+
+        var errors = validator.validate(data, model);
+
+        test.expect(1);
+        test.ok(errors.valid);
+
+        test.done();
+    },
+    validateDateFormatFailed: function(test) {
+        var data = {
+            "salutation": "Mr Death",
+            "dateOfBirth": "2014-1-1"
+        };
+        var model = {
+            properties: {
+                dateOfBirth: {
+                    type: "string",
+                    format: 'date'
+                }
+            }
+        };
+
+        var errors = validator.validate(data, model);
+
+        test.expect(1);
+        test.ok(!errors.valid);
+
+        test.done();
+    },
+
+    validateDateTime: function(test) {
+        var data = {
+            "salutation": "Mr Death",
+            "dateOfBirth": "2014-01-01T12:00:00"
+        };
+        var model = {
+            properties: {
+                dateOfBirth: {
+                    type: "string",
+                    format: 'date'
+                }
+            }
+        };
+
+        var errors = validator.validate(data, model);
+
+        test.expect(3);
+        test.ok(!errors.valid);
+        test.ok(errors.errorCount === 1);
+        test.ok(errors.errors[0].message === 'dateOfBirth (2014-01-01T12:00:00) is not a type of date', errors.errors[0].message);
+
+        test.done();
     }
 };
