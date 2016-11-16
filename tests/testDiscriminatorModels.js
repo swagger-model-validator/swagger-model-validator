d9883874c285a8dc436de1ac1dcac851e7ccefb7
Brody Dunn <brody.dunn@atlantishealthcare.com>
2016-11-16 13:39:29 +1300

    Move discriminator support into validate so it supports all models and submodels of a validate request.

--- tests/testDiscriminatorModels.js
+++ tests/testDiscriminatorModels.js
@@ -62,5 +62,122 @@ module.exports.refTests = {
         test.expect(1);
         test.ok(errors.valid);
         test.done();
+    },
+    hasRefWithDefinitionPrefixUsingValidateTest: function(test) {
+        var data = {
+            sample: true,
+            location: {
+                top: 1,
+                left: 1,
+                right: 5,
+                bottom: 5
+            },
+            type: "dataModel"
+        };
+
+        var models = {
+            firstModel: {
+                discriminator: "type"
+            },
+            dataModel: {
+                required: [ "sample" ],
+                properties: {
+                    sample: {
+                        type: "boolean"
+                    },
+                    location: {
+                        $ref: "#/definitions/Location"
+                    }
+                }
+            },
+            Location: {
+                required: [ "top", "left" ],
+                properties: {
+                    top: {
+                        type: "integer"
+                    },
+                    left: {
+                        type: "integer"
+                    },
+                    right: {
+                        type: "integer"
+                    },
+                    bottom: {
+                        type: "integer"
+                    }
+                }
+            }
+        };
+
+        var swagger = {
+            allModels: models
+        };
+
+        var testValidator = new Validator(swagger);
+
+        var errors =validator.validate(data, models["firstModel"], models, false, true);
+
+        test.expect(1);
+        test.ok(errors.valid);
+        test.done();
+    },
+    hasRefWithDefinitionPrefixUsingValidateWithMultpleErrorsTest: function(test) {
+        var data = {
+            sample: true,
+            location: {
+                left: 1,
+                right: 5,
+                bottom: 5
+            },
+            type: "dataModel"
+        };
+
+        var models = {
+            firstModel: {
+                discriminator: "type"
+            },
+            dataModel: {
+                required: [ "sample" ],
+                properties: {
+                    sample: {
+                        type: "boolean"
+                    },
+                    location: {
+                        $ref: "#/definitions/Location"
+                    }
+                }
+            },
+            Location: {
+                required: [ "top", "left" ],
+                properties: {
+                    top: {
+                        type: "integer"
+                    },
+                    left: {
+                        type: "integer"
+                    },
+                    right: {
+                        type: "integer"
+                    },
+                    bottom: {
+                        type: "integer"
+                    }
+                }
+            }
+        };
+
+        var swagger = {
+            allModels: models
+        };
+
+        var testValidator = new Validator(swagger);
+
+        var errors =validator.validate(data, models["firstModel"], models, false, true);
+
+        test.expect(3);
+        test.ok(!errors.valid);
+        test.ok(errors.errorCount == 1, errors.errorCount);
+        test.ok(errors.errors[0].message == "top is a required field", errors.errors[0].message);
+        test.done();
     }
 };
