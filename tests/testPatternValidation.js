e9b91aceebe8af85b8830cb265379717f65f188a
julianpellasrice <julian.pellas-rice@telia.no>
2017-12-15 08:56:27 +0100

    Added a test using Swagger field name restrictions

--- tests/testPatternValidation.js
+++ tests/testPatternValidation.js
@@ -1,6 +1,3 @@
-/**
- * Created by bdunn on 10/11/2014.
- */
 var Validator = require('../lib/modelValidator');
 var validator = new Validator();
 
@@ -66,6 +63,38 @@ module.exports.patternTests = {
 		test.expect(2);
 		test.ok(!errors.valid);
 		test.ok(errors.errors[0].message === 'restrictedField does not match the pattern [A-Z]{3}', 'message: ' + errors.errors[0].message);
+		test.done();
+	},
+	doesNotMatchSwaggerFieldNameRestrictions: function(test) {
+		var data = {
+			"patternType":{"restrictedField": "AB!"}
+		};
+
+		var models = {
+			"MyType": {
+				"type": "object",
+				"properties": {
+					"patternType": {
+						"$ref": "#\/definitions\/PatternType"
+					}
+				}
+			},
+			"PatternType": {
+				"type": "object",
+				"properties": {
+					"restrictedField": {
+						"type": "string",
+						"pattern": "^[a-zA-Z0-9\.\-_]+$"
+					}
+				}
+			}
+		};
+
+		var errors = validator.validate(data, models["MyType"], models);
+
+		test.expect(2);
+		test.ok(!errors.valid);
+		test.ok(errors.errors[0].message === 'restrictedField does not match the pattern ^[a-zA-Z0-9\.\-_]+$', 'message: ' + errors.errors[0].message);
 		test.done();
 	},
 	doesNotMatchPatternLength: function(test) {
