5d6ee6c821efa19a74eba62042d40223dc82bd34
Brody Dunn <brody.dunn@atlantishealthcare.com>
2018-08-15 10:33:05 +1200

    Add extra tests to verify #104 is not an issue.

--- tests/testPatternValidation.js
+++ tests/testPatternValidation.js
@@ -128,5 +128,124 @@ module.exports.patternTests = {
 		test.ok(!errors.valid);
 		test.ok(errors.errors[0].message === 'restrictedField does not match the pattern ^[A-Z]{3,3}$', 'message: ' + errors.errors[0].message);
 		test.done();
-	}
+	},
+	patternTestGauravve: function(test) {
+		var models = {
+            "user": {
+                "title": "User",
+                "type": "object",
+                "additionalProperties": false,
+                "properties": {
+                    "pNumber": {
+                        "type": "string",
+                        "minLength": 3,
+                        "maxLength": 30,
+                        "pattern": "[A-Za-z0-9-]*"
+                    },
+                    "org": {
+                        "type": "string",
+                        "minLength": 3,
+                        "maxLength": 30,
+                        "pattern": "[A-Za-z0-9-]"
+                    },
+                    "enabled": {
+                        "type": "boolean"
+                    },
+                    "userRoles": {
+                        "type": "array",
+                        "uniqueItems": true,
+                        "minItems": 1,
+                        "maxItems": 7,
+                        "items": {
+                            "type": "string",
+                            "enum": [
+                                "admin",
+                                "consumer"
+                            ]
+                        }
+                    }
+                },
+                "required": [
+                    "pNumber",
+                    "org"
+                ],
+            }
+        };
+
+        var data =   {
+            "pNumber": "p!@#$!@#",
+        	"org": "!@#$!@#$",
+            "enabled": true,
+            "userRoles": [
+				"admin",
+				"consumer"
+			]
+    	};
+
+    	var errors = validator.validate(data, models["user"], models);
+
+        test.expect(2);
+        test.ok(!errors.valid);
+        test.equal("org does not match the pattern [A-Za-z0-9-]", errors.errors[0].message);
+		test.done();
+	},
+    patternTestGauravve2: function(test) {
+        var models = {
+            "user": {
+                "title": "User",
+                "type": "object",
+                "additionalProperties": false,
+                "properties": {
+                    "pNumber": {
+                        "type": "string",
+                        "minLength": 3,
+                        "maxLength": 30,
+                        "pattern": "[A-Za-z0-9-]*"
+                    },
+                    "org": {
+                        "type": "string",
+                        "minLength": 3,
+                        "maxLength": 30,
+                        "pattern": "[A-Za-z0-9-]"
+                    },
+                    "enabled": {
+                        "type": "boolean"
+                    },
+                    "userRoles": {
+                        "type": "array",
+                        "uniqueItems": true,
+                        "minItems": 1,
+                        "maxItems": 7,
+                        "items": {
+                            "type": "string",
+                            "enum": [
+                                "admin",
+                                "consumer"
+                            ]
+                        }
+                    }
+                },
+                "required": [
+                    "pNumber",
+                    "org"
+                ],
+            }
+        };
+
+        var data =   {
+            "pNumber": "p!@#$!@#",
+            "org": "Actual99",
+            "enabled": true,
+            "userRoles": [
+                "admin",
+                "consumer"
+            ]
+        };
+
+        var errors = validator.validate(data, models["user"], models);
+
+        test.expect(1);
+        test.ok(errors.valid);
+        test.done();
+    }
 };
