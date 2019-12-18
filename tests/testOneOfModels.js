af9b795027fe3b5bb2fb157a07550c31ecfa155d
Youva Boumedine <youva.boumedine@montreal.ca>
2019-12-18 08:23:36 -0500

    removed prettier

--- tests/testOneOfModels.js
+++ tests/testOneOfModels.js
@@ -190,4 +190,123 @@ module.exports.refTests = {
         test.ok(errors.valid);
         test.done();
     },
+
+    hasOneOfReferenceWhenRightTest: function(test) {
+      var data = {
+        messageType: "Test",
+        message: {
+          client_id: {
+            id: 90,
+            property2: "asdf"
+          }
+        }
+      };
+      var models = {
+        dataModel: {
+          openapi: "3.0.0",
+          info: {
+            title: "example-service",
+            description: "example swagger file to show problem",
+            version: "3.0.0"
+          },
+          servers: [
+            {
+              url: "/api/v1"
+            }
+          ],
+          paths: {
+            "/some/endpoint": {
+              post: {
+                summary: "validate and then do some stuff",
+                requestBody: {
+                  content: {
+                    "application/json": {
+                      schema: {
+                        $ref: "#/components/schemas/exampleSchema"
+                      }
+                    }
+                  }
+                },
+                responses: {
+                  "200": {
+                    description: "success"
+                  },
+                  "400": {
+                    description: "Data error",
+                    content: {
+                      "application/json": {
+                        schema: {
+                          type: "object"
+                        }
+                      }
+                    }
+                  },
+                  "500": {
+                    description: "Internal server error"
+                  }
+                }
+              }
+            }
+          },
+          components: {
+            schemas: {
+              exampleSchema: {
+                description: "Request payload",
+                required: ["messageType", "message"],
+                properties: {
+                  messageType: {
+                    type: "string",
+                    description: "Type of request to api",
+                    example: "someMessageType"
+                  },
+                  message: {
+                    type: "object",
+                    required: ["client_id"],
+                    properties: {
+                      client_id: {
+                        oneOf: [
+                          {
+                            $ref: "#/definitions/clientnumber"
+                          },
+                          {
+                            $ref: "#/definitions/clientobject"
+                          }
+                        ]
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        clientnumber: {
+          description: "Client Id Number",
+          type: "number"
+        },
+        clientobject: {
+          description: "Client Id Object",
+          type: "object",
+          properties: {
+            id: {
+              type: "number"
+            },
+            property2: {
+              type: "string"
+            }
+          }
+        }
+      };
+  
+      var errors = validator.validate(
+        data,
+        models["dataModel"].components.schemas.exampleSchema,
+        models
+      );
+  
+      console.log(errors);
+      test.expect(1);
+      test.ok(errors.valid);
+      test.done();
+    }
 };
